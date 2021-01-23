df0fd5f5881ee4899453d0ad4f8b94dbb4d0fd4c
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-01-23 12:39:31 +0000

    Add cors and express-pino-logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
+import { Application } from 'express';
 import bodyParser from 'body-parser';
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
-import { Application } from 'express';
 import * as database from '@src/database';
 import { UsersController } from './controllers/users';
 import logger from './logger';
@@ -21,6 +23,12 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.app.use(expressPino({ logger }));
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupControllers(): void {
